0c7c7036e6085e964e49abe337e39d22a5ac2093
SergeyV1S <v1s.coop@yandex.ru>
2025-07-31 03:02:11 +0500

    re-initialize project

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Card, CardContent, CardHeader } from "@shared/ui";
+
+export default function Home() {
+  return (
+    <div className=''>
+      <Card>
+        <CardHeader>Привет</CardHeader>
+        <CardContent>Привет еще</CardContent>
+      </Card>
+    </div>
+  );
+}
